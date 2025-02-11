718fe33db30d3e10798aeee0889dd2ffb7c49c62
Jennifer Ross <pguczol23@yandex.ru>
2025-02-11 04:29:39 +0300

    feat: Update tailwind.config.js to include improved color scheme and font
    This commit message succinctly captures:
    - Enhanced primary color
    definition
    - Added CSS variables for better customization
    - New
    "poppins" font addition
    - Improved typography configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,50 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  content: [
-    './app/**/*.php',
-    './config/**/*.php',
-    './resources/**/*.{php,js}',
-    './storage/framework/views/*.php',
-  ],
-  theme: {
-    extend: {
-      colors: {
-        primary: colors.blue,
-      },
-    },
-  },
-  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+	content: [
+		'./app/**/*.php',
+		'./config/**/*.php',
+		'./resources/**/*.{php,js}',
+		'./storage/framework/views/*.php',
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary: {
+					DEFAULT: 'var(--primary-color)',
+					light: 'var(--primary-color)',
+					dark: 'var(--primary-color)',
+					50: '#4D4884',
+					100: '#464176',
+					200: '#3E3A69',
+					300: '#36335C',
+					400: '#2E2B45',
+					500: '#272442',
+					600: '#1F1D35',
+					700: 'var(--primary-color)',
+					800: '#0F0E1A',
+					900: '#08070D',
+				},
+			},
+			text: {
+
+			},
+			gridTemplateColumns: {
+				// Column grid
+				'16': 'repeat(16, minmax(0, 1fr))',
+				'6': 'repeat(6, minmax(0, 1fr))',
+				'4': 'repeat(4, minmax(0, 1fr))',
+			},
+			maxWidth: {
+				'8xl': '96rem',
+			},
+			boxShadow: {
+				'3lg': '0px 25px 35px 0 rgb(0 0 0 / 0.03), 0px 0px 5px 0 rgb(0 0 0 / 0.05)',
+			},
+			fontFamily: {
+				"poppins": ['Poppins', 'sans-serif']
+			}
+		},
+	},
+	plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 }
