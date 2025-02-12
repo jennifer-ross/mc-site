9e94647159c1af2ab4aa35680b0119b3dc5e36b8
Jennifer Ross <pguczol23@yandex.ru>
2025-02-12 21:11:16 +0300

    feat: Add Intersection Observer to handle visibility for apply animations

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,10 @@
-import {
-  Livewire,
-  Alpine,
-} from '../../vendor/livewire/livewire/dist/livewire.esm'
+import { Alpine, Livewire } from '../../vendor/livewire/livewire/dist/livewire.esm'
 
 import Tooltip from '@ryangjchandler/alpine-tooltip'
+import './echo.js'
+import { Observer } from './intersect.js'
 
 Alpine.plugin(Tooltip)
 
 Livewire.start()
-
-import './echo.js'
+Observer.start()
---
+++ resources/js/intersect.js
@@ -0,0 +1,64 @@
+const Observer = {
+	start() {
+		if (document.readyState === 'loading') {
+			document.addEventListener('DOMContentLoaded', () => this.observe())
+
+			return
+		}
+
+		this.observe()
+	},
+
+	observe() {
+		const selectors = [
+			'[class*=" intersect:"]',
+			'[class*=":intersect:"]',
+			'[class^="intersect:"]',
+			'[class="intersect"]',
+			'[class*=" intersect "]',
+			'[class^="intersect "]',
+			'[class$=" intersect"]',
+		]
+
+		document.querySelectorAll(selectors.join(',')).forEach(element => {
+			let lastTime = 0
+
+			const observer = new IntersectionObserver(entries => {
+				entries.forEach(entry => {
+					if (lastTime + 200 >= entry.time) {
+						return
+					}
+
+					if (!entry.isIntersecting) {
+						element.setAttribute('no-intersect', '')
+
+						return
+					}
+
+					lastTime = entry.time
+					element.removeAttribute('no-intersect')
+
+					element.classList.contains('intersect-once') && observer.disconnect()
+				})
+			}, {
+				threshold: this.getThreshold(element),
+			})
+
+			observer.observe(element)
+		})
+	},
+
+	getThreshold(element) {
+		if (element.classList.contains('intersect-full')) {
+			return 0.99
+		}
+
+		if (element.classList.contains('intersect-half')) {
+			return 0.5
+		}
+
+		return 0
+	},
+}
+
+export { Observer }
