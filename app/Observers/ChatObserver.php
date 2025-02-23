3e48449ff318c4dcdd5b535d8132e1b6b093b3f5
Jennifer Ross <pguczol23@yandex.ru>
2025-02-23 18:35:41 +0300

    style: Change spaces

--- app/Observers/ChatObserver.php
+++ app/Observers/ChatObserver.php
@@ -2,52 +2,49 @@
 
 namespace App\Observers;
 
+use App\Enums\ChatType;
 use App\Enums\ChatUserRole;
 use App\Models\Chat;
 
 class ChatObserver
 {
-    /**
-     * Handle the Chat "created" event.
-     */
-    public function created(Chat $chat): void
-    {
-		$chat->participants()->create([
-			'chat_id' => $chat->id,
-			'user_id' => $chat->owner_id,
-			'role' => ChatUserRole::Admin
-		]);
-    }
+	/**
+	 * Handle the Chat "created" event.
+	 */
+	public function created(Chat $chat): void
+	{
+		$chat->addUser($chat->owner_id, $chat->type !== ChatType::Private ? ChatUserRole::Admin : ChatUserRole::Member);
+	}
 
-    /**
-     * Handle the Chat "updated" event.
-     */
-    public function updated(Chat $chat): void
-    {
-        //
-    }
+	/**
+	 * Handle the Chat "updated" event.
+	 */
+	public function updated(Chat $chat): void
+	{
+		//
+	}
 
-    /**
-     * Handle the Chat "deleted" event.
-     */
-    public function deleted(Chat $chat): void
-    {
-        //
-    }
+	/**
+	 * Handle the Chat "deleted" event.
+	 */
+	public function deleted(Chat $chat): void
+	{
+		//
+	}
 
-    /**
-     * Handle the Chat "restored" event.
-     */
-    public function restored(Chat $chat): void
-    {
-        //
-    }
+	/**
+	 * Handle the Chat "restored" event.
+	 */
+	public function restored(Chat $chat): void
+	{
+		//
+	}
 
-    /**
-     * Handle the Chat "force deleted" event.
-     */
-    public function forceDeleted(Chat $chat): void
-    {
-        //
-    }
+	/**
+	 * Handle the Chat "force deleted" event.
+	 */
+	public function forceDeleted(Chat $chat): void
+	{
+		//
+	}
 }
