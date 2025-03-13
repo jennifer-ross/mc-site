116538545c8ee09db2a6fa7329162e6c72508f4c
Jennifer Ross <pguczol23@yandex.ru>
2025-03-13 16:56:47 +0300

    refactor: Scripts section

--- resources/views/components/scripts.blade.php
+++ resources/views/components/scripts.blade.php
@@ -1,37 +1,3 @@
-@stack('scripts')
 <script src="{{ asset('js/vendors/lazyload.min.js') }}"></script>
-<script>
-	window.modalsToInit = []
-	document.addEventListener('DOMContentLoaded', e => {
-		window.lazyLoadInstance = new LazyLoad({});
-	})
-
-	const observeDOM = (function() {
-		let MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
-
-		return function(obj, callback) {
-			if (!obj || obj.nodeType !== 1) {
-				return;
-			}
-
-			if (MutationObserver) {
-				// define a new observer
-				let mutationObserver = new MutationObserver(callback);
-
-				// have the observer observe for changes in children
-				mutationObserver.observe(obj, {childList: true, subtree: true});
-				return mutationObserver;
-			} else if (window.addEventListener) { // browser support fallback
-				obj.addEventListener('DOMNodeInserted', callback, false);
-				obj.addEventListener('DOMNodeRemoved', callback, false);
-			}
-		}
-	})();
-	window.observeDOM = observeDOM
-
-	observeDOM(document.body, e => {
-		if (window.lazyLoadInstance) {
-			window.lazyLoadInstance.update();
-		}
-	})
-</script>
+@vite(['resources/js/modules/lazyload.js', 'resources/js/app.js'])
+@stack('scripts')
